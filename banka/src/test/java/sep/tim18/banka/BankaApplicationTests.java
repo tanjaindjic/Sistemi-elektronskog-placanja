528859929d03a861cae3fb24004b63a6f7012f4f
tanjaindjic <mali.patuljko@gmail.com>
2018-11-30 19:10:30 +0100

    dodati projekte u foldere
    here we go

---
+++ banka/src/test/java/sep/tim18/banka/BankaApplicationTests.java
@@ -0,0 +1,16 @@
+package sep.tim18.banka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BankaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
