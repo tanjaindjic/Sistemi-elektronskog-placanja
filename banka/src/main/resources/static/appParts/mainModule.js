64c1e4ec1e57d855c27a1f22eb79c762a6c0e6c1
tanjaindjic <mali.patuljko@gmail.com>
2018-12-01 10:05:00 +0100

    doterana stranica za placanje

--- banka/src/main/resources/static/appParts/mainModule.js
+++ banka/src/main/resources/static/appParts/mainModule.js
@@ -2,51 +2,11 @@ var mainModule = angular.module('mainModule', [ 'ui.router', 'ngStorage', 'angul
 
 mainModule.config(function($stateProvider, $urlRouterProvider) {
 
-    $urlRouterProvider.otherwise('/home');
+    $urlRouterProvider.otherwise('/');
 
     $stateProvider.state('home', {
-        url: '/home',
+        url: '/',
         templateUrl : 'appParts/centerComponent/center.html',
-    })
-    .state('login', {
-        url: '/login',
-        templateUrl: 'appParts/loginComponent/login.html',
-        controller : 'loginController'
-    })
-    .state('klijent', {
-        url: '/klijent',
-        templateUrl: 'appParts/klijentComponent/klijentHome.html',
-        controller : 'klijentHomeController'
-    })
-    .state('racuni', {
-        url: '/racuni',
-        templateUrl: 'appParts/klijentIzvodiComponent/klijentIzvodi.html',
-        controller : 'klijentIzvodiController'
-    })
-    .state('nalog', {
-        url: '/nalog',
-        templateUrl: 'appParts/nalogComponent/nalog.html',
-        controller : 'nalogController'
-    })
-    .state('noviRacun', {
-        url: '/noviRacun',
-        templateUrl: 'appParts/kreirajRacunComponent/kreirajRacun.html',
-        controller : 'kreirajRacunController'
-    })
-    .state('upravljanjeRacunima', {
-        url: '/upravljanjeRacunima',
-        templateUrl : 'appParts/upravljanjeRacunimaComponent/upravljanjeRacunima.html',
-        controller : 'upravljanjeRacunimaController'
-    })    
-    .state('izvestaji', {
-        url: '/izvestaji',
-        templateUrl : 'appParts/izvestajiComponent/izvestaji.html',
-        controller : 'izvestajiController'
-    })
-    .state('importNaloga', {
-        url: '/importNaloga',
-        templateUrl : 'appParts/importComponent/importComponent.html',
-        controller : 'importController'
     })
 
 });
