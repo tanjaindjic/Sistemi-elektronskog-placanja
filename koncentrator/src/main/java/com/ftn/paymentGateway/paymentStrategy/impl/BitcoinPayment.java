e3e10d5446f4e485c980f37e4ee37f3ac021b6f8
mjok <marijajoksimovic@hotmail.com>
2019-02-03 11:29:37 +0100

    Periodicna sinhronizacija transakcija, bitcoin

--- koncentrator/src/main/java/com/ftn/paymentGateway/paymentStrategy/impl/BitcoinPayment.java
+++ koncentrator/src/main/java/com/ftn/paymentGateway/paymentStrategy/impl/BitcoinPayment.java
@@ -2,6 +2,7 @@ package com.ftn.paymentGateway.paymentStrategy.impl;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -16,6 +17,9 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestClientException;
@@ -27,6 +31,7 @@ import com.ftn.paymentGateway.dto.TransakcijaIshodDTO;
 import com.ftn.paymentGateway.enumerations.IdPoljePlacanja;
 import com.ftn.paymentGateway.enumerations.TransakcijaStatus;
 import com.ftn.paymentGateway.exceptions.PaymentErrorException;
+import com.ftn.paymentGateway.exceptions.TransactionUpdateExeption;
 import com.ftn.paymentGateway.exceptions.UnsupportedMethodException;
 import com.ftn.paymentGateway.model.PodrzanoPlacanje;
 import com.ftn.paymentGateway.model.PoljePodrzanoPlacanje;
@@ -102,7 +107,7 @@ public class BitcoinPayment implements PaymentStrategy{
 	}
 	
 	@Override
-	//@Scheduled(initialDelay = 10000, fixedRate = 30000)
+	@Scheduled(initialDelay = 10000, fixedRate = 1200000) // Odlozi 10s, ponavljaj na 20min
 	public void syncDB() {
 		
 		List<Transakcija> pending = null;
@@ -114,37 +119,37 @@ public class BitcoinPayment implements PaymentStrategy{
 			return;
 		}
 		
-		HttpHeaders headers = new HttpHeaders();
 		RestTemplate restTemplate = new RestTemplate();
 			
 		for(Transakcija temp : pending) {
 			
 			PodrzanoPlacanje accountInfo = podrzanoPlacanjeRepository.findByEntitetPlacanjaAndTipPlacanja(temp.getEntitetPlacanja(), bitcoinPaymentType).get(0);
 			
+			HttpHeaders headers = new HttpHeaders();
 			headers.set("Authorization", "Token "+getIdNaloga(accountInfo.getPolja()));
 			
-			System.out.println("Token "+getIdNaloga(accountInfo.getPolja()));
-			
-			HttpEntity<?> bitcoinRequest = new HttpEntity<>(null, headers);
+			HttpEntity<?> bitcoinRequest = new HttpEntity<>(headers);
 		    ResponseEntity<String> bitcoinResponse = null;
 		    
 		    try {
-				bitcoinResponse = restTemplate.exchange(new URI("https://api.coingate.com/v2/orders/"+temp.getIzvrsnaTransakcija()), HttpMethod.GET, bitcoinRequest, String.class);
-			} catch (RestClientException | URISyntaxException e) {
+				bitcoinResponse = restTemplate.exchange("https://api-sandbox.coingate.com/v2/orders/"+temp.getIzvrsnaTransakcija(), HttpMethod.GET, bitcoinRequest, String.class);
+			} catch (RestClientException e) {
 				System.out.println("Greska prilikom sinhronizacije zahteva, BITCOIN.");
-				e.printStackTrace();
-				
+				//e.printStackTrace();
+				break;
 			}
-		    
-		    System.out.println(bitcoinResponse);
 		    
 		    JsonParser basicJsonParser = new BasicJsonParser();
 		    Map<String, Object> paymentInfo = basicJsonParser.parseMap(bitcoinResponse.getBody());
-			
-		    for (Map.Entry<String, Object> entry : paymentInfo.entrySet())
-		    {
-		        System.out.println(entry.getKey() + "/" + entry.getValue());
-		    }
+		    
+		    try {
+				changeStatus(temp, bindStatus(paymentInfo.get("status").toString()));
+			} catch (TransactionUpdateExeption e) {
+				System.out.println("Greska prilikom promene statusa transakcije, BITCOIN.");
+				//e.printStackTrace();
+				break;
+			}
+		    
 		}
 	}
 
@@ -165,6 +170,43 @@ public class BitcoinPayment implements PaymentStrategy{
 		}
 		
 		return retVal;
+	}
+	
+	@Transactional(readOnly = false, rollbackFor = Exception.class, propagation = Propagation.REQUIRED, isolation = Isolation.SERIALIZABLE)
+	private void changeStatus(Transakcija transakcija, TransakcijaStatus newStatus) throws TransactionUpdateExeption {
+		
+		if(transakcija == null || newStatus == null) {
+			throw new TransactionUpdateExeption();
+		}
+		
+		if(transakcija.getStatus() != TransakcijaStatus.C || newStatus == TransakcijaStatus.E || newStatus == TransakcijaStatus.C){
+			throw new TransactionUpdateExeption();
+		}
+		
+		transakcija.setStatus(newStatus);
+		transakcija = transakcijaRepository.save(transakcija);
+		
+		System.out.println("USPESNO! "+transakcija.getIzvrsnaTransakcija()+" "+transakcija.getStatus());
+	}
+	
+	private TransakcijaStatus bindStatus(String statusCode) {
+		
+		switch (statusCode) {
+			case "new" :
+	        	return TransakcijaStatus.C;
+			case "pending" :
+	        	return TransakcijaStatus.C;
+	        case "paid":
+	        	return TransakcijaStatus.U;
+	        case "invalid" :
+	        	return TransakcijaStatus.N;
+	        case "canceled" :
+	        	return TransakcijaStatus.N;
+	        case "expired" :
+	        	return TransakcijaStatus.E;
+		}
+
+		return null;
 	}
 
 }
