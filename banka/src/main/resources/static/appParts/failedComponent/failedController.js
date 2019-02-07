42d266313375ec6ecda644f0665c92a7b4741e90
tanjaindjic <mali.patuljko@gmail.com>
2019-02-07 14:51:40 +0100

    ispravljena trasakcija

--- banka/src/main/resources/static/appParts/failedComponent/failedController.js
+++ banka/src/main/resources/static/appParts/failedComponent/failedController.js
@@ -1,7 +1,7 @@
 mainModule.controller('failedController', ['$scope', '$window', 'mainService',
-       function($scope, $window, mainService){
+             function($scope, $window, mainService){
 
 
 
-       }
-   ]);
+             }
+         ]);
